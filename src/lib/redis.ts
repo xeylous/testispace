7742d33e431e5ce7fa955bccfdb21928cb7309d3
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2026-01-23 18:54:27 +0530

    redis optimiazation is done

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -6,6 +6,37 @@ if (!REDIS_URL) {
     throw new Error('Please define the REDIS_URL environment variable inside .env.local');
 }
 
-const redis = new Redis(REDIS_URL);
+const getRedisClient = () => {
+    if (process.env.NODE_ENV === 'development') {
+        // In development mode, use a global variable so that the value
+        // is preserved across module reloads caused by HMR (Hot Module Replacement).
+        // @ts-ignore
+        if (!global.redis) {
+            // @ts-ignore
+            global.redis = new Redis(REDIS_URL, {
+                maxRetriesPerRequest: null, // Allow infinite retries or handle in logic
+                retryStrategy(times) {
+                    const delay = Math.min(times * 50, 2000);
+                    return delay;
+                },
+                connectTimeout: 10000, // 10 seconds
+            });
+        }
+        // @ts-ignore
+        return global.redis;
+    }
+
+    // In production mode, it's best to not use a global variable.
+    return new Redis(REDIS_URL, {
+        maxRetriesPerRequest: null,
+        retryStrategy(times) {
+            const delay = Math.min(times * 50, 2000);
+            return delay;
+        },
+        connectTimeout: 10000,
+    });
+};
+
+const redis = getRedisClient();
 
 export default redis;
